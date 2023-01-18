8fc20329dedc584677a81166b513198bd2fb31d5
TwiggyDaniels <ssleder@iastate.edu>
2023-01-18 08:59:24 -0600

    No batching after all, matrix multiplication seems to be working and all the shapes are correct at least. Hopefully this is right?

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pseudo-differential-operators
-A fan implementation of the graph convolutional layer in "Parameterized Pseudo-Differential Operators for Graph Convolutional Neural Networks" by Potter, et. al
+A fan implementation of the graph convolutional layer in [*Parameterized Pseudo-Differential Operators for Graph Convolutional Neural Networks*](https://ieeexplore.ieee.org/document/9607762) by Potter, et. al
+
+## Installation
+
+1. Install requirements
+```
+pip3 install -r requirements.txt
+```
--- ppdo_conv.py
+++ ppdo_conv.py
@@ -6,11 +6,11 @@ class PPDOConv2D(MessagePassing):
     def __init__(self, in_channels, out_channels, norm_factor=None):
         # mean propagation per the original paper
         super().__init__(aggr='mean')
-        self.lin = Linear(, out_channels)
+        self.lin = Linear(in_channels*3, out_channels)
 
-
-    def forward(self, x, edge_index):
+    def forward(self, x, edge_index, pos):
         x = self.propagate(edge_index, x=x, pos=pos)
+        x = self.lin(x)
         return x
 
     def message(self, x_i, x_j, pos_i, pos_j):
@@ -22,11 +22,20 @@ class PPDOConv2D(MessagePassing):
         # no need to take root since we square it when we use it anyways
         dist = torch.sum(pos_diff, dim=-1)
 
-        msg = torch.squeeze((pos_diff * feat_diff ) / dist), -2)
+        # I want to multiply each row of the feature matrix by the 
+        # corresponding x & y row of the positional differences...
+        # There MUST be an easier way to do this...
+        # I could also unsqueeze but that basically does the same
+        # thing as what I am already doing... Damn there must be
+        # a one-line way to do this...
+        part_x = pos_diff[:,0][:,None] * feat_diff
+        part_y = pos_diff[:,1][:,None] * feat_diff
 
-        msg = torch.cat([msg, x_i], dim=1)
+        # Same as the last bigass comment
+        part_x /= dist[:, None]
+        part_y /= dist[:, None]
 
-        return msg
+        return torch.cat([part_x, part_y, x_i], dim=-1)
 
     def update(self, aggr_out):
         return aggr_out
